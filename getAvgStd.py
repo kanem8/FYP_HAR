22fc1593a3607e158f38b58252b360c271d8eedc
kanem8 <mark.kane8@mail.dcu.ie>
2019-03-11 14:52:24 +0000

    find avg and std dev

--- getAvgStd.py
+++ getAvgStd.py
@@ -38,7 +38,7 @@ train_transform = transforms.Compose([
     # transforms.Resize((100, 40)), # original size: (288, 432)
     # transforms.Resize((40, 100)), # original size: (288, 432)
     transforms.ToTensor(),
-    transforms.Normalize([0.9671], [0.0596]) # unsure how to normalize the tensor correctly
+    # transforms.Normalize([0.9671], [0.0596]) # unsure how to normalize the tensor correctly
 ])
 
 # use the same transform for the validation data
@@ -135,8 +135,8 @@ Validation_loader_HR = DataLoader(Validation_set_HR, batch_size=50, num_workers=
 
 count = 0
 for batch, targets in train_loader_imu1:
-    # if count > 1:
-    #     break
+    if count > 10:
+        break
     mean = batch.mean()
     print("mean = {}".format(mean))
     std_dev = batch.std()
