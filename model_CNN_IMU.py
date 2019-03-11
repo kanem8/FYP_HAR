9dd17f0fac2a4405800ba81adc33a18d109c9ed5
kanem8 <mark.kane8@mail.dcu.ie>
2019-03-11 14:24:05 +0000

    cnn-imu bigger lr

--- model_CNN_IMU.py
+++ model_CNN_IMU.py
@@ -281,7 +281,7 @@ model = CNN_IMU()
 model.to(device)
 
 # optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, nesterov=True)
-optimizer = optim.RMSprop(model.parameters(), lr=0.00001, alpha=0.95)
+optimizer = optim.RMSprop(model.parameters(), lr=0.0001, alpha=0.95)
 
 def save_checkpoint(optimizer, model, epoch, filename):
     checkpoint_dict = {
